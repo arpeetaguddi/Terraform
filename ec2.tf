b9f1e0cd062e5ec5e0f9473f9fafb3a30dc47b55
arpeetaguddi <arpeetaguddi839@gmail.com>
2026-07-13 16:59:06 +0530

    cc

--- ec2.tf
+++ ec2.tf
@@ -1,10 +1,16 @@
-resource "aws_instance" "webserver" {
-  key_name               = var.this_key_name
-  ami                    = var.this_ami
-  instance_type          = var.This_instance_type
-  vpc_security_group_ids = [var.This_vpc_security_group_ids]
-  count                  = var.this_count
+provider "aws" {
+  region = "ap-south-1"
+}
 
+resource "aws_instance" "webserver" {
+  key_name                = var.this_key_name
+  ami                     = var.this_ami
+  instance_type           = var.This_instance_type
+
+  subnet_id               = "subnet-0fa7af11bd33fa518"
+  vpc_security_group_ids  = [var.This_vpc_security_group_ids]
+
+  count                   = var.this_count
   disable_api_termination = var.this_disable_api_termination
 
   tags = {
--- var.tf
+++ var.tf
@@ -1,23 +1,23 @@
 variable "this_key_name" {
-   default = "arpeeta" 
-
+  default = "ggg"
 }
 
 variable "this_ami" {
-   default = "ami-0b910d1016287a5e7 " 
+  default = "ami-0b910d1016287a5e7"
 }
-
 
 variable "This_instance_type" {
-    default  = "t3.micro"
+  default = "t3.micro"
 }
 
-variable "This_vpc_security_group_ids" {default = "sg-00c0c76b8f2d5a64a"}
+variable "This_vpc_security_group_ids" {
+  default = "sg-0aaacc470803228b7"
+}
 
 variable "this_count" {
-    default = 4
+  default = 4
 }
 
 variable "this_disable_api_termination" {
-    default =  false
+  default = false
 }
