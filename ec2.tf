8fe69696567c2ff0bb6da883c78d1a7cd16259f0
arpeetaguddi <arpeetaguddi839@gmail.com>
2026-07-13 16:15:12 +0530

    m

--- ec2.tf
+++ ec2.tf
@@ -4,9 +4,16 @@ provider "aws" {
 }
 
 # 2. Launch a minimal EC2 Instance
-resource "aws_instance" "minimal_ec2" {
-  ami           = "ami-01a18c38ece67e620" 
-  instance_type = "t2.micro"
+  resource "aws_instance" "webserver" {
+   depends_on = [aws_security_group.webserversg] #explict dependency
+   key_name = var.this_key_name
+   ami = var.this_ami 
+   instance_type = var.This_instance_type
+   vpc_security_group_ids = [var.This_vpc_security_group_ids , aws_security_group.webserversg.id , data.aws_security_groups.defaultsg_search.id ]
+   count = var.this_count
+   
+   disable_api_termination = var.this_disable_api_termination
+
 
   tags = {
     Name = "Basic-EC2"
